8bb4971d17b78aec68d6520608f63fde7244219f
jjin70 <jjin0434@naver.com>
2025-05-06 03:50:52 +0900

    스프링 프로젝트 세팅

---
+++ back/src/main/java/gradude/springVision/SpringVisionApplication.java
@@ -0,0 +1,13 @@
+package gradude.springVision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVisionApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/gradude/springVision/SpringVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package gradude.springVision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
